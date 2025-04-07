f400bfe346ef298efd2922fb722b02d8ed4b7903
clashernitin <124627314+clashernitin@users.noreply.github.com>
2025-04-07 07:38:34 +0000

    comparison class

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log("2"> 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null != 0);
+
+/* an equality check == and comparison >, <, >=, <= works differently. Comparison convert null to a number treating it as 0 that's why
+null>=0 is true and null>0 is false. */
+
+//  === strict check
+
+console.log("2" === 2);
+
+
+
+
