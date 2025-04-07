bfa295fde1354cfb3adc160c16d792a8fca03439
clashernitin <124627314+clashernitin@users.noreply.github.com>
2025-04-07 07:27:54 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,10 @@
-// let score = "string"
+let score = "string"
 // 
 // console.log(typeof score);
 // console.log(typeof(score));
 // 
-// let valueInNumber = Number(score);
+
+let valueInNumber = Number(score);
 // 
 // console.log(typeof valueInNumber);
 // 
@@ -14,9 +15,9 @@
 //  true => 1
 //  false => 0
 
-// let isLoggedIn = ""
-// 
-// let booleanIsLoggedIn = Boolean(isLoggedIn);
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 // 
 // console.log (typeof booleanIsLoggedIn);
 // 
@@ -25,11 +26,51 @@
 //  1 => true; 0=> false
 //  "" => false ; "nitin" => true
 
-// let codingDays = 1
-// 
-// let stringCodingDays = String(codingDays);
+
+let codingDays = 1
+
+let stringCodingDays = String(codingDays);
 // 
 // console.log(typeof stringCodingDays);
 // 
 // console.log(stringCodingDays);
+
+// *****************************Operations******************************
+
+let value = 3
+
+let negValue = -value
+
+// 
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Nitin"
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1"+ 2);
+// 
+// console.log(1 + "2");
+// 
+// console.log("1" + 2 + 2);
+// 
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 =num3 = 2 + 2
+
+let gameCounter = 100;
+
+gameCounter++;
+
+console.log(gameCounter);
+
 
