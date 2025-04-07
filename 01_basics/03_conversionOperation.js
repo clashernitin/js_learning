02fb910a6da08353ea918430bed2c3cad97de3bf
clashernitin <124627314+clashernitin@users.noreply.github.com>
2025-04-07 07:14:08 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "string"
+// 
+// console.log(typeof score);
+// console.log(typeof(score));
+// 
+// let valueInNumber = Number(score);
+// 
+// console.log(typeof valueInNumber);
+// 
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1
+//  false => 0
+
+// let isLoggedIn = ""
+// 
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// 
+// console.log (typeof booleanIsLoggedIn);
+// 
+// console.log(booleanIsLoggedIn);
+
+//  1 => true; 0=> false
+//  "" => false ; "nitin" => true
+
+// let codingDays = 1
+// 
+// let stringCodingDays = String(codingDays);
+// 
+// console.log(typeof stringCodingDays);
+// 
+// console.log(stringCodingDays);
+
