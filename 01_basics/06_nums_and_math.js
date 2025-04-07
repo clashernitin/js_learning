57a4cc02260cbb1984883a9a969eedd34d7ff5f2
clashernitin <124627314+clashernitin@users.noreply.github.com>
2025-04-07 11:25:19 +0000

    math & Num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,39 @@
+const score = 400
+
+// console.log(score);
+
+
+const balance = new Number(400);
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const num2 = 99.6753
+
+// console.log(num2.toPrecision(4));
+
+const num3 = 100000000
+// console.log(num3.toLocaleString('en-IN'));
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-8973));
+// 
+// console.log(Math.round(4.9));
+// 
+// console.log(Math.ceil(89.6));
+// 
+// console.log(Math.floor(89.9));
+
+const min = 10
+
+const max = 20
+
+let randomValue = Math.random()* (max - min  + 1) + min
+console.log(Math.floor(randomValue)) 
+
+
+
