83c96b29f2adda81c132fe6712dc2f5107de7657
clashernitin <124627314+clashernitin@users.noreply.github.com>
2025-04-07 07:54:40 +0000

    data types summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,9 @@
+//  Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100      
+
+// Non Primitive or Reference Type
+
+// Array, Objects, Functions
