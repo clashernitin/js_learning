7072468bd3357de919e3b4ca3d35c2f61e807a26
clashernitin <124627314+clashernitin@users.noreply.github.com>
2025-04-07 11:01:02 +0000

    datatype summary + strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,28 @@
+const name = "Nitin"
+
+const repoCount = 50
+
+// console.log(name + repoCount);
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+// console.log(name[0]);
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.charAt(4));
+// console.log(name.indexOf('N') , name.indexOf('n'));
+
+const newString = name.substring(0,2);
+// console.log(newString);
+
+const anotherString = name.slice(2,3 );
+// console.log(anotherString);
+
+let fName = " Raman         "
+console.log(fName + "!");
+console.log(fName.trim() + "!");
+
+let fullName = "Raman-is-a-good-boy"
+
+console.log(fullName.split('-'));
--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -7,3 +7,29 @@ const score = 100
 // Non Primitive or Reference Type
 
 // Array, Objects, Functions
+
+// ******************************************************
+
+//  Stack Memory (Primitive) => we get a copy when we use stack i.e., any change made is made to the copy and not the original value, Heap Memory (Non Primitive) => we get a reference to the original value in heap so if any change is made, the original value changes
+
+let myName = "Nitin"
+
+let anotherName = myName
+
+anotherName = "Saxena"
+
+// console.log(myName);
+// 
+// console.log(anotherName);
+
+let user1 = {
+    email: "ns@gmail.com",
+    upi: "1234@paytm",
+}
+
+let user2 = user1
+
+user2.email = "rst@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
