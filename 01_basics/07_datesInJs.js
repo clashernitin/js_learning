dd50b514787f19c20c650da8f6b174b6076d0fb7
clashernitin <124627314+clashernitin@users.noreply.github.com>
2025-04-07 18:30:45 +0000

    Dates

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,27 @@
+//  Dates
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+
+let randomDate = new Date(2025, 0, 23, 5, 3)
+
+// console.log(randomDate.toLocaleString());
+
+let newDate = new Date ("2025-05-18")
+// console.log(newDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+
+let newerDate = new Date()
+console.log(newerDate);
+
+
+
