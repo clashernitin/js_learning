b4fcc6a67bfe6c4b102a7a08f3311d4e9f5e5920
clashernitin <124627314+clashernitin@users.noreply.github.com>
2025-04-07 07:02:20 +0000

    added dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+4) // we are using nodejs, not browser
+
+console.log(2+2);
+
+let name = "Nitin"
+let age = 24
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => (standalone value) => is "object" when checked with typeof
+// undefined => value is not defined but variable is declared
+// symbol => unique
+
+// object
+
+console.log(typeof undefined)
